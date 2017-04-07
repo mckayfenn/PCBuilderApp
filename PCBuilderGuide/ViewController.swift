4e1035cbe7a07cadcf1ba2db5c8c8cf369514bf6
u0939404 <u0939404@kcmac-45.scl.utah.edu>
2017-04-07 14:27:35 -0600

    First commit/initalize

---
+++ PCBuilderGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PCBuilderGuide
+//
+//  Created by u0939404 on 4/7/17.
+//  Copyright © 2017 Fenn/Phelps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
